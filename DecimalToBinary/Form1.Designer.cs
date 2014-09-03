81e43d751147ea43abf4271e4cbe4f2ae0fca540
CodingCodeChen <peter3598768@gmail.com>
2014-09-03 19:02:33 +0800

    decimal to binary

---
+++ DecimalToBinary/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace DecimalToBinary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Number = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Answer = new System.Windows.Forms.Label();
+            this.DTB = new System.Windows.Forms.RadioButton();
+            this.BTD = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Number
+            // 
+            this.Number.Location = new System.Drawing.Point(12, 68);
+            this.Number.Name = "Number";
+            this.Number.Size = new System.Drawing.Size(100, 22);
+            this.Number.TabIndex = 2;
+            this.Number.TextChanged += new System.EventHandler(this.Number_TextChanged);
+            this.Number.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Number_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(147, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Answer
+            // 
+            this.Answer.AutoSize = true;
+            this.Answer.Location = new System.Drawing.Point(12, 93);
+            this.Answer.Name = "Answer";
+            this.Answer.Size = new System.Drawing.Size(0, 12);
+            this.Answer.TabIndex = 5;
+            // 
+            // DTB
+            // 
+            this.DTB.AutoSize = true;
+            this.DTB.Checked = true;
+            this.DTB.Location = new System.Drawing.Point(12, 12);
+            this.DTB.Name = "DTB";
+            this.DTB.Size = new System.Drawing.Size(112, 16);
+            this.DTB.TabIndex = 7;
+            this.DTB.TabStop = true;
+            this.DTB.Text = "Decimal To Binary";
+            this.DTB.UseVisualStyleBackColor = true;
+            // 
+            // BTD
+            // 
+            this.BTD.AutoSize = true;
+            this.BTD.Location = new System.Drawing.Point(12, 34);
+            this.BTD.Name = "BTD";
+            this.BTD.Size = new System.Drawing.Size(112, 16);
+            this.BTD.TabIndex = 8;
+            this.BTD.Text = "Binary To Decimal";
+            this.BTD.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Number";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 211);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BTD);
+            this.Controls.Add(this.DTB);
+            this.Controls.Add(this.Answer);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Number);
+            this.Name = "Form1";
+            this.Text = "DecimalToBinary";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Number;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Answer;
+        private System.Windows.Forms.RadioButton DTB;
+        private System.Windows.Forms.RadioButton BTD;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DecimalToBinary/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DecimalToBinary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string temp = Number.Text;
+            int enter = Convert.ToInt32(temp);
+
+            if (DTB.Checked)    //10轉2
+            {
+                int tempNum = DecimalToBinary(enter);
+                Answer.Text = "Answer : " + tempNum;
+            }
+            else
+            {
+                //2轉10
+                int tempNum = BinaryToDecimal(enter);
+                Answer.Text = "Answer : " + tempNum;
+            }
+        }
+
+        private int DecimalToBinary(int number)
+        {
+            int total = 0, m = 1;
+
+            while (number != 0)
+            {
+                total = total + (number % 2) * m;
+                number = number / 2;
+                m = m * 10;
+
+            }
+            return total;
+        }
+
+        private int BinaryToDecimal(int number)
+        {
+            int temp = 0, i = 0, m = 1;
+
+            while (number > 0)
+            {
+
+                temp = temp + m * power(2, i);
+                m = number % 10;
+                number = number / 10;
+                i++;
+
+            }
+            return temp;
+        }
+
+        private int power(int a,int b)  //a的b次方的函式
+        {
+            int Base=1,i;
+
+            for(i=0;i<b;i++)
+            {
+                Base=Base*a;
+            }
+            return Base;
+        }
+
+        private void Number_TextChanged(object sender, EventArgs e)
+        {
+            Answer.Text = "";
+            if (Number.Text == "")
+            {
+                button1.Enabled = false;
+            }
+            else
+            {
+                button1.Enabled = true;
+            }
+        }
+
+        private void Number_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsDigit(e.KeyChar) || Char.IsControl(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Number.ShortcutsEnabled = false;
+        }
+    }
+}
